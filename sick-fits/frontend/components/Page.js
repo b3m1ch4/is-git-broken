8c398b5642cae6d4f0aa9300f51d0f67f752f14b
b3m1ch4 <b3.m1ch4@gmail.com>
2018-11-09 12:08:09 -0500

    Module 2: Lessons 5, 6 and 7
    Introduction styled components
    Using Themes and styled components
    Inline vs global styling
    Updated header with styling
    Included Meta tag and logo

--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,14 +1,69 @@
 import React, { Component } from 'react';
+import styled, { ThemeProvider, injectGlobal } from 'styled-components';
 import Header from './Header.js';
+import Meta from '../components/Meta';
 
-export default class Page extends Component {
+const theme = {
+  red: '#FF0000',
+  black: '#393939',
+  grey: '#3a3a3a',
+  lightgrey: '#e1e1e1',
+  offwhite: '#ededed',
+  maxWidth: '1000px',
+  bs: '0 12px 24px 0 rgba(0, 0, 0, 0.00)',
+};
+
+const StyledPage = styled.div`
+  background: white;
+  color: ${props => props.theme.black};
+`;
+
+const Inner = styled.div`
+  max-width: ${props => props.theme.maxWidth};
+  margin: 0 auto;
+  padding: 2rem;
+`;
+
+injectGlobal`
+@font-face {
+  font-family: 'radnika_next';
+  src: url('/static/radnikanext-medium-webfont.woff2')
+  format('woff2');
+  font-weight: normal;
+  font-style: normal;
+}
+  html {
+    box-sizing: border-box;
+    font-size: 10px;
+  }
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+  body {
+    padding: 0;
+    margin: 0;
+    font-size: 1.5rem;
+    line-height: 2;
+    font-family: 'radnika_next';
+  }
+  a {
+    text-decoration: none;
+    color: ${theme.black};
+  }
+`;
+
+class Page extends Component {
   render() {
     return (
-        <div>
+        <ThemeProvider theme={theme}>
+        <StyledPage>
+        <Meta />
         <Header />
-        <p>Hey, I'm the page component.</p>
-        {this.props.children}
-        </div>
+        <Inner>{this.props.children}</Inner>
+        </StyledPage>
+        </ThemeProvider>
     )
   }
 }
+
+export default Page;
