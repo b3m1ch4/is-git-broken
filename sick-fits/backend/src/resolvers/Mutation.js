2a74b1abf0b57c45e9c00105b1f2593ea8018d61
b3m1ch4 <b3.m1ch4@gmail.com>
2018-11-19 16:43:53 -0500

    Modules 13 and 14
    Test GraphQL queries on the backend

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,8 @@
-const Mutations = {};
+const Mutations = {
+  createDog(parent, args, ctx, info) {
+    // create a doggo
+  console.log('arguments are', args);
+},
+};
 
 module.exports = Mutations;
--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,3 +1,8 @@
-const Query = {};
+const Query = {
+  dogs(parent, args, ctx, info) {
+    return [{name: 'Basil'}, {name: 'Georgia'}]
+  }
+
+};
 
 module.exports = Query;
