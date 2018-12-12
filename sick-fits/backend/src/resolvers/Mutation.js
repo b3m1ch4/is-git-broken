8a6f938423462b83363efa4f55286997aa7f62cc
b3m1ch4 <b3.m1ch4@gmail.com>
2018-12-12 16:46:00 -0500

    Wrote update query
    GQL Query for specific item
    Mutation updates the item without disrupting the ID
    Schema updated accordingly

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -14,6 +14,19 @@ const Mutations = {
     return item;
 
   },
+  updateItem(parent, args, ctx, info) {
+    // first take copy of the arguments
+    const updates = {...args};
+    delete updates.id;
+    return ctx.db.mutation.updateItem({
+      data: updates,
+      where: {
+        id: args.id
+      }
+    },
+    info
+    );
+  }
 }
 
 module.exports = Mutations;
